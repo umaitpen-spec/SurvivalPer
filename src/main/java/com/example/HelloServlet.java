c2d3467c49a939bff04982165607d2b72a732b4a
zs-gsch07 <janaanandan2007@gmail.com>
2026-05-12 19:03:16 +0530

    first commit

---
+++ src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<body>");
+        out.println("<h1>Hello from Servlet!!!</h1>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
